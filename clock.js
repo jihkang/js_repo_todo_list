e898dc4b4ab13359d3e066e6f838023b255c9c45
jihkang <72780016+jihkang@users.noreply.github.com>
2021-02-12 23:02:04 +0900

    Add files via upload

--- clock.js
+++ clock.js
@@ -2,8 +2,9 @@
 // button or event listener 를 통해 데이터를 변화시킬 예정.
 // 아직 미구현.
 // 전역 변수로 설정되어 있는 녀석들도 제거 하여 구현할 예정.
-const clock = document.querySelector(".js-form");
-const clockContainer = clock.querySelector("h4");
+const clock = document.querySelector(".js-form"),
+  clockTexter = clock.querySelector("h1");
+  clockContainer = clock.querySelector("h4");
 
 const hour_24 = true;
 
@@ -19,19 +20,31 @@ function setTime(txt, afternoon) {
     }
 }
 
-function getTime() {
+function changedDay(time) {
+    if (time.getHours() == 0 && time.getSecond() == 0) {
+      return true;
+    } else {
+      return false;
+    }
+}
+
+function getTime(target) {
     var time = new Date();
 
     var hour = time.getHours();
     var minute = time.getMinutes();
     var timeAfter = true;
 
+    if (changedDay(time)) {
+      remainDate(target - time);
+    }
+
     minute = minute < 10 ? ' 0' + minute : ' ' + minute;
     hour = hour < 10 ? ' 0' + hour : ' ' + hour;
-    
+
     if (hour_24 == true) {
         console.log(hour + ':' + minute);
-    } else { 
+    } else {
         if (hour < 12) {
             timeAfter = true;
         } else {
@@ -42,8 +55,18 @@ function getTime() {
     setTime(hour + ' : ' + minute, timeAfter);
 }
 
+function remainDate(target) {
+    target = target / (1000 * 60 * 60 * 24);
+    console.log(target);
+    target = Math.ceil(target);
+    clockTexter.innerText = target + "remain";
+}
+
 function init() {
-    getTime();
+    targetDate = new Date("2022-06-14:00:00:00+0900");
+
+    remainDate(targetDate - new Date());
+    getTime(targetDate);
     timer = setInterval(getTime, 1000);
 }
 
